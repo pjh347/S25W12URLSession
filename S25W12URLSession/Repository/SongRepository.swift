89aca2186f65d686b0929950a07be659f7ede6c2
pjh347 <gownj333@gmail.com>
2025-11-24 16:40:07 +0900

    🗃️addSong

--- S25W12URLSession/Repository/SongRepository.swift
+++ S25W12URLSession/Repository/SongRepository.swift
@@ -1,4 +1,5 @@
-
 protocol SongRepository: Sendable {
     func fetchSongs() async throws -> [Song]
+    
+    func saveSong(_ song: Song) async throws
 }
--- S25W12URLSession/ViewModel/SongViewModel.swift
+++ S25W12URLSession/ViewModel/SongViewModel.swift
@@ -11,10 +11,26 @@ final class SongViewModel {
 
     private var _songs: [Song] = []
     var songs: [Song] { _songs }
-    
+
     var path = NavigationPath()
 
     func loadSongs() async {
         _songs = try! await repository.fetchSongs()
+    }
+    
+    // func addSong(newSong song: Song)
+    // newSong은 외부 매개변수 이름, song은 함수 내부에서 사용하는 매개변수 이름
+    // viewModel.addSong(newSong: song)
+    //
+    // _는 외부 매개변수 이름을 사용하지 않겠다는 표현
+    // viewModel.addSong(song)
+    func addSong(_ song: Song) async {
+        do {
+            try await repository.saveSong(song)
+            _songs.append(song)
+        }
+        catch {
+            debugPrint("에러 발생: \(error)")
+        }
     }
 }
